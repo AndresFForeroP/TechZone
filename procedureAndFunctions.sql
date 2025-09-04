0419032a8e6a5f1b8e460bc2810242fa4b50c1cf
AndresFForeroP <andres2200500@correo.uis.edu.co>
2025-09-04 13:15:47 -0500

    feat: :sparkles: Agregados dos procesos almacenados

--- procedureAndFunctions.sql
+++ procedureAndFunctions.sql
@@ -1,2 +1,73 @@
-CREATE OR REPLACE FUNCTION procedimiento ()
-RETURNS ;
+--Un procedimiento almacenado para registrar una venta.
+CREATE OR REPLACE PROCEDURE registrar_nueva_venta(
+    cliente_id INTEGER,
+    p_product_ids INTEGER[],
+    p_quantities INTEGER[],
+    p_prices FLOAT8[]
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    new_order_id INTEGER;
+BEGIN
+    INSERT INTO ventas (cliente_id, fecha)
+    VALUES (cliente_id, NOW())
+    RETURNING id INTO new_order_id;
+
+    FOR i IN 1..array_length(p_product_ids, 1) LOOP
+        INSERT INTO ventas_detalle (venta_id, producto_id, cantidad, precio_unitario)
+        VALUES (new_order_id, p_product_ids[i], p_quantities[i], p_prices[i]);
+    END LOOP;
+
+    RAISE NOTICE 'Orden creada con ID: %', new_order_id;
+END;
+$$;
+
+CALL registrar_nueva_venta(
+    1,
+    ARRAY[1, 2, 3],
+    ARRAY[2, 1, 5],
+    ARRAY[10.50, 20.00, 5.75]
+);
+
+SELECT 
+    v.id AS venta_id,
+    v.cliente_id,
+    v.fecha,
+    d.producto_id,
+    d.cantidad,
+    d.precio_unitario,
+    (d.cantidad * d.precio_unitario) AS subtotal
+FROM 
+    ventas v
+JOIN 
+    ventas_detalle d ON v.id = d.venta_id
+ORDER BY 
+    v.id, d.id;
+
+--Validar que el cliente exista.
+
+CREATE OR REPLACE PROCEDURE existencia_cliente(
+    cliente_id INTEGER
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    existe BOOLEAN;
+BEGIN
+    SELECT EXISTS (
+        SELECT 1 FROM clientes WHERE id = cliente_id
+    ) INTO existe;
+
+    IF NOT existe THEN
+        RAISE EXCEPTION 'El cliente con ID % no existe', cliente_id;
+    ELSE
+        RAISE NOTICE 'El cliente con ID % sí existe', cliente_id;
+    END IF;
+END;
+$$;
+
+
+CALL existencia_cliente(1);
+
+--Verificar que el stock sea suficiente antes de procesar la venta.
--- queries.sql
+++ queries.sql
@@ -3,3 +3,44 @@
 
 SELECT * FROM productos
 WHERE stock < 5;
+
+2️⃣ Calcular ventas totales de un mes específico.
+
+SELECT v.fecha , SUM(vd.precio_unitario) AS ventas 
+FROM ventas_detalle AS vd
+JOIN ventas AS v ON v.id = vd.venta_id 
+WHERE v.fecha BETWEEN '2025-08-01' AND '2025-8-30'
+GROUP BY v.fecha ;
+
+3️⃣ Obtener el cliente con más compras realizadas.
+
+SELECT c.nombre, COUNT(v.id) as compras
+FROM clientes as c
+JOIN ventas AS v ON v.cliente_id = c.id
+GROUP BY c.nombre
+ORDER BY compras DESC
+LIMIT 1;
+
+4️⃣ Listar los 5 productos más vendidos.
+
+SELECT p.nombre AS producto, COUNT(vd.id) AS ventas
+FROM productos AS p
+JOIN ventas_detalle AS vd ON vd.producto_id = p.id
+GROUP BY producto
+ORDER BY ventas DESC
+LIMIT 5;
+
+5️⃣ Consultar ventas realizadas en un rango de fechas de tres Días y un Mes.
+
+SELECT v.fecha , SUM(vd.precio_unitario) AS ventas 
+FROM ventas_detalle AS vd
+JOIN ventas AS v ON v.id = vd.venta_id 
+WHERE v.fecha BETWEEN '2025-08-01' AND '2025-9-03'
+GROUP BY v.fecha ;
+
+6️⃣ Identificar clientes que no han comprado en los últimos 6 meses.
+
+SELECT c.nombre AS cliente, v.fecha
+FROM clientes AS c
+JOIN ventas AS v ON c.id = v.cliente_id
+WHERE v.fecha <= NOW() - INTERVAL '6 months';
